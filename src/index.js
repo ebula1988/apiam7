d2d6b39a136d6e02399563f1500a37e6cb7c62cc
Edgar Romero Bula <edgaromerobula@gmail.com>
2022-08-10 22:42:48 -0500

    se modifica el port para que tome el del servidor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import app from "./app";
 
 const main = ()=>{
-    app.listen(app.get("port"));
-    console.log(`Server on port ${app.get("port")}`);
+    app.listen(process.env.PORT || app.get("port"));
+    console.log(`Server on port ${process.env.PORT ||app.get("port")}`);
 };
 
 main();
